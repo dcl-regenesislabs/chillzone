a7f14751bdf735b7131f763dc9c6cd8ca9c1ca75
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2026-07-28 12:38:53 -0300

    ft: add social models

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {} from '@dcl/sdk/math'
-import { engine } from '@dcl/sdk/ecs'
+import { engine, pointerEventsSystem, InputAction } from '@dcl/sdk/ecs'
+import { openExternalUrl } from '~system/RestrictedActions'
 import { setupUi } from './ui'
+import { EntityNames } from '../assets/scene/entity-names'
 
 
 export function main() {
@@ -8,5 +10,31 @@ export function main() {
     //setupUi()
 
     // your scene code here
+
+    const smartphone = engine.getEntityOrNullByName(EntityNames.SmartPhone)
+    if (smartphone) {
+        pointerEventsSystem.onPointerDown(
+            {
+                entity: smartphone,
+                opts: { button: InputAction.IA_POINTER, hoverText: 'Follow Decentraland on X' },
+            },
+            () => {
+                openExternalUrl({ url: 'https://x.com/decentraland' })
+            }
+        )
+    }
+
+    const laptop = engine.getEntityOrNullByName(EntityNames.Laptop)
+    if (laptop) {
+        pointerEventsSystem.onPointerDown(
+            {
+                entity: laptop,
+                opts: { button: InputAction.IA_POINTER, hoverText: 'Connect with the Community' },
+            },
+            () => {
+                openExternalUrl({ url: 'https://discord.gg/6RvBZYXFBf' })
+            }
+        )
+    }
 }
 
